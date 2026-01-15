ac2fdd021ecfeeb5710acceb73aa2eab05937b2d
Diego Trejo <diego_trejo@hotmail.com>
2026-01-15 07:42:13 -0500

    uso de vistas

---
+++ Zooologico.MVC/Controllers/EspeciesController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Zoologico.ApiConsumer;
+using Zoologico.Modelos;
+
+namespace Zooologico.MVC.Controllers
+{
+    public class EspeciesController : Controller
+    {
+        // GET: EspeciesController
+        public ActionResult Index()
+        {
+            var data = Crud<Especie>.ReadAll();
+            var totoal = data.Data.Count;
+            ViewBag.Total = totoal;
+            ViewData["Total"] = totoal;
+            ViewBag.Promedio = data.Data.Average(e => e.Codigo);
+            ViewBag.Minimo = data.Data.Min(e => e.Codigo);
+            ViewBag.Maximo = data.Data.Max(e => e.Codigo);
+
+            return View(data.Data);
+        }
+
+        // GET: EspeciesController/Details/5
+        public ActionResult Details(int id)
+        {
+            var data = Crud<Especie>.ReadBy("codigo", id.ToString());
+            return View(data.Data);
+        }
+
+        // GET: EspeciesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EspeciesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Especie data)
+        {
+            try
+            {
+                Crud<Especie>.Create(data);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EspeciesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var data = Crud<Especie>.ReadBy("codigo", id.ToString());
+            return View(data.Data);
+        }
+
+        // POST: EspeciesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Especie data)
+        {
+            try
+            {
+                Crud<Especie>.Update(id.ToString(), data);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EspeciesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var data = Crud<Especie>.ReadBy("codigo", id.ToString());
+            return View(data.Data);
+        }
+
+        // POST: EspeciesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Especie data)
+        {
+            try
+            {
+                Crud<Especie>.Delete(id.ToString());
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Zooologico.MVC/Program.cs
@@ -0,0 +1,41 @@
+using Zoologico.ApiConsumer;
+using Zoologico.Modelos;
+
+namespace Zooologico.MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Crud<Especie>.UrlBase = "http://10.147.19.119/api/Especies";
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
